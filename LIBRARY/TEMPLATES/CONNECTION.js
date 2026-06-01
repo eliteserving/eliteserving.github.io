43712fabcfc337f9e85f098c5927ec9069ed4640
connectionserver <erimrank167@gmail.com>
2026-06-01 08:20:17 +0300

    Updates

---
+++ LIBRARY/TEMPLATES/CONNECTION.js
@@ -0,0 +1,5 @@
+import { APPMODE } from "./DATABASE/FUNCTIONS/APPMODE/index.js";
+import { BODIED } from "./DATABASE/FUNCTIONS/BODIED/index.js";
+import { CLEAR } from "./DATABASE/FUNCTIONS/CLEAR/index.js";
+
+export{APPMODE,BODIED,CLEAR};
