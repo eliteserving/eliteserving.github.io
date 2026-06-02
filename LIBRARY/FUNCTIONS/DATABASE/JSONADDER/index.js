681a3d8befb1ff44cfc1b7121313e9eba041ad6b
connectionserver <erimrank167@gmail.com>
2026-06-02 23:59:41 +0300

    Updates

--- LIBRARY/FUNCTIONS/DATABASE/JSONADDER/index.js
+++ LIBRARY/FUNCTIONS/DATABASE/JSONADDER/index.js
@@ -1,3 +1,10 @@
-export const JSONADDER=()=>{
-
+export const JSONADDER = (DATA,NEWDATA,CALLBACK) => {
+    try {
+        let CurrentData =Array.isArray(DATA)?DATA:[DATA];
+        let AddedData =Array.isArray(NEWDATA)?NEWDATA:[NEWDATA];
+        const Result = [...CurrentData,...AddedData];
+        CALLBACK(Result);
+    } catch {
+        CALLBACK([]);
+    };
 };
