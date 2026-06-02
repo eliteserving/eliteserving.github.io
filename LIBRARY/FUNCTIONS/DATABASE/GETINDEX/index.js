8ad7ef4315d0f1eb9de0394ede575352e980b193
connectionserver <erimrank167@gmail.com>
2026-06-02 23:56:32 +0300

    Updates

--- LIBRARY/FUNCTIONS/DATABASE/GETINDEX/index.js
+++ LIBRARY/FUNCTIONS/DATABASE/GETINDEX/index.js
@@ -1,3 +1,31 @@
-export const GETINDEX=()=>{
-
+export const GETINDEX = (DATABASE,STORE,CALLBACK) => {
+    try {
+        const Request =indexedDB.open(DATABASE);
+        Request.onsuccess = () => {
+            const DB =Request.result;
+            if (
+                !DB.objectStoreNames.contains(STORE)
+            ) {
+                CALLBACK(JSON.stringify([]));
+                DB.close();
+                return;
+            }
+            const Transaction =DB.transaction(STORE, "readonly");
+            const ObjectStore =Transaction.objectStore(STORE);
+            const GetRequest =ObjectStore.getAll();
+            GetRequest.onsuccess = () => {
+                CALLBACK(JSON.stringify(GetRequest.result));
+                DB.close();
+            };
+            GetRequest.onerror = () => {
+                CALLBACK(JSON.stringify([]));
+                DB.close();
+            };
+        };
+        Request.onerror = () => {
+            CALLBACK(JSON.stringify([]));
+        };
+    } catch {
+        CALLBACK(JSON.stringify([]));
+    };
 };
