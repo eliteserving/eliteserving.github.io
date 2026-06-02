cf7bf12a8aac428bbd676152401d8bbe0183df14
connectionserver <erimrank167@gmail.com>
2026-06-02 23:38:08 +0300

    Updates

--- LIBRARY/FUNCTIONS/DATABASE/FETCH/index.js
+++ LIBRARY/FUNCTIONS/DATABASE/FETCH/index.js
@@ -2,7 +2,8 @@ export const FETCH=(LINK,DATA,CALLBACK)=>{
     ONLINE(()=>{
         fetch(LINK,{
             mode:"cors",
-            method:"POST"
+            method:"POST",
+            body: JSON.stringify(DATA)
         })
         .then(res =>res.json())
         .then(Data =>{
