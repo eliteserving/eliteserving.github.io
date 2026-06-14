9d4d4f4d607b36fb3f81d84aeba1632108e8be49
eliteserving <erimrank167@gmail.com>
2026-06-14 18:47:47 +0300

    Updates

--- BUILD/NOORFASHION/WEB/index.js
+++ BUILD/NOORFASHION/WEB/index.js
@@ -1,4 +1,4 @@
-const LINK="https://eliteserving.github.io/LIBRARY/DEMO/Data.json";
+const LINK="https://docs.google.com/spreadsheets/d/1D7Km39gogTyE1i991NJ9E0jxYBzlhga2vuDpHv4rNk8/edit?usp=sharing";
 
 const MOBILEVIEW=()=>{
 
