3ca5e9ed68d4fdcef2ab038ceafc6b4cf29e7e15
connectionserver <erimrank167@gmail.com>
2026-06-15 07:42:24 +0300

    Updates

--- CONSOLE/index.js
+++ CONSOLE/index.js
@@ -1,1 +1,11 @@
-Ks
+// CREATE DATABASE
+//  https://script.google.com/macros/s/AKfycbzzV6EHb-mFnFm_lS3p5CEKTzMjGVr1bl9mEEbOMm1-t7YvEwWWG2IxKph_OGUppxrH/exec
+
+// CREATE TABLE
+// https://script.google.com/macros/s/AKfycbzS-M9Q2tPzFWyMTc3vmvKwtXG1YCRuQbplcLZxr5XKYmSoKOjFztrHujqGD--VKwGYkQ/exec
+
+// INSERT DATA
+// https://script.google.com/macros/s/AKfycbzENDLW1MWLgUPsed5wC13xhRl0s8B1CoXXR36XOTg9ulbULBzB81nzq0NG2Y1alBYuUA/exec
+
+// GET DATA
+// 
