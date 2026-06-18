90f0d6be36a7f30da4760119150390274447dc8a
eliteserving <erimrank167@gmail.com>
2026-06-18 08:33:44 +0300

    Updates

--- LIBRARY/PLUGINS/DATABASE/ANDROID/SMS/index.js
+++ LIBRARY/PLUGINS/DATABASE/ANDROID/SMS/index.js
@@ -1,12 +1,14 @@
-
-export const GETSMS = (callback)=>{
+export const GETSMS = (CALLBACK)=>{
     CONDITION(localStorage.getItem("ENV") === "ANDROID",()=>{
         if(!window.AndroidSMS){callback([]);return;}
         const json =AndroidSMS.getAllSms();
         const sms =JSON.parse(json);
-        callback(sms);
+        CALLBACK(sms);
     },()=>{
-        REDUX("../")
-
+        FETCH("https://eliteserving.github.io/LIBRARY/DEMO/Sms.json","",(Data)=>{
+            REDUX(Data,(DATA)=>{
+                CALLBACK(DATA);
+            });
+        });
     });
 };
