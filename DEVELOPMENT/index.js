87cc94fe35e3558e21c79b7b0ee97956465978d3
eliteserving <erimrank167@gmail.com>
2026-06-18 07:40:29 +0300

    Updates

--- DEVELOPMENT/index.js
+++ DEVELOPMENT/index.js
@@ -18,6 +18,21 @@ const MOBILEVIEW=()=>{
     
     });
 
+    VIEWS("",(DATA)=>{
+
+        POSITION(DATA,"absolute");
+        TOP(DATA,"50px");
+        HEIGHT(DATA,"auto");
+        BOTTOM(DATA,"50px");
+
+        GETSMS((DATATA)=>{
+
+            DISPLAY(DATA,DATATA);
+
+        });
+
+    });
+
     FOOTER("",(DATA)=>{
 
         HEADER(DATA,"All",()=>{
