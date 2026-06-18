ee5af88e49fafc3d99de374986b32289a3fe147b
eliteserving <erimrank167@gmail.com>
2026-06-18 08:17:34 +0300

    Updates

--- LIBRARY/PLUGINS/DATABASE/ANDROID/SMS/index.js
+++ LIBRARY/PLUGINS/DATABASE/ANDROID/SMS/index.js
@@ -2,9 +2,5 @@ export const GETSMS = (callback)=>{
     if(!window.AndroidSMS){callback([]);return;}
     const json =AndroidSMS.getAllSms();
     const sms =JSON.parse(json);
-    JSONIFICATION(sms,(Data)=>{
-        REDUX(Data,(Daata)=>{
-            callback(Daata);
-        });
-    });
+    callback(sms);
 };
