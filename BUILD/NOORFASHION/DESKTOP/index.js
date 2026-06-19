2bf51018a041caab4cb166877a339bc6787b3ef8
eliteserving <erimrank167@gmail.com>
2026-06-19 19:10:18 +0300

    Updates

---
+++ BUILD/NOORFASHION/DESKTOP/index.js
@@ -0,0 +1,75 @@
+
+
+const LINK="https://docs.google.com/spreadsheets/d/1D7Km39gogTyE1i991NJ9E0jxYBzlhga2vuDpHv4rNk8/edit?usp=sharing";
+
+const MOBILEVIEW=()=>{
+
+    CLEAR("");
+
+    VIEWS("",(HOLDER)=>{
+
+        HEIGHT(HOLDER,"");
+
+        HEAD(HOLDER,(DATA)=>{
+    
+            ICONS(DATA,BLACKMENUICON,WHITEMENUICON,()=>{
+    
+            });
+    
+            HEADER(DATA,"ShylanSiraj Fashion Hub",()=>{
+    
+            });
+    
+            ICONS(DATA,WHITESHOPPINGCART,WHITESHOPPINGCART,()=>{
+    
+            });
+    
+        });
+
+        INLINEVIEW(HOLDER,(DATA)=>{
+
+            HEIGHT(DATA,"50px");
+
+            BUTTON(DATA,"ALL",(DATATED)=>{
+
+                MARGIN(DATATED,"");
+
+                WIDTH(DATATED,"50px");
+
+                HEIGHT(DATATED,"32px");
+            
+            });
+
+            INLINEVIEW(DATA,(DATITED)=>{
+
+                BACKGROUND(DATITED,"red");
+
+                WIDTH(DATITED,"70%");
+
+            });
+
+        });
+
+        VIEWS(HOLDER,(DATA)=>{
+
+            HEIGHT(DATA,"");
+
+            BACKGROUND(DATA,"Blue");
+
+        });
+    
+    });
+  
+};
+
+const DESKTOPVIEW=()=>{
+
+    CLEAR("");
+
+    HEADER("","NCOO",()=>{
+
+    });
+    
+};
+
+VIEW(()=>{MOBILEVIEW();},()=>{DESKTOPVIEW()});
