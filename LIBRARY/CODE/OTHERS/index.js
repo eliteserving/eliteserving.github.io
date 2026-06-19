891c91ad0a4133a4e376ffb2a05dd427fc27fab1
eliteserving <erimrank167@gmail.com>
2026-06-19 10:11:33 +0300

    Updates

---
+++ LIBRARY/CODE/OTHERS/index.js
@@ -0,0 +1,17 @@
+const MOBILEVIEW=()=>{
+
+    HEADER("","MOBILE",()=>{
+
+    });
+
+};
+
+const DESKTOPVIEW=()=>{
+
+    HEADER("","DESKTOP",()=>{
+
+    });
+
+};
+
+VIEW(()=>{MOBILEVIEW();},()=>{DESKTOPVIEW()});
