6c606c5e04102bcc0f23af9acf6cfccc95ae9046
eliteserving <erimrank167@gmail.com>
2026-06-23 07:44:09 +0300

    Updates

--- LIBRARY/CODE/Connection.js
+++ LIBRARY/CODE/Connection.js
@@ -1,3 +1,18 @@
+import { PROJECT } from "./OTHERS/PROJECT/Connection.js";
 export const FEATURES=()=>{
-
+    const TITLE = document.title;
+    if (TITLE === "EliteBuilder") {
+        PROJECT();
+    } else {
+        switch (TITLE) {
+            case "ANDROID":import(BUILDLINK+NAME+"/ANDROID/index.js");
+                break;
+            case "DESKTOP":import(BUILDLINK+NAME+"/DESKTOP/index.js");
+                break;
+            case "WEB":import(BUILDLINK+NAME+"/WEB/index.js");
+                break;
+            default:import("../DEVELOPMENT/index.js");
+                break;
+        };  
+    };
 };
