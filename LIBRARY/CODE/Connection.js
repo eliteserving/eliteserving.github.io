9e74636994ac8c9b8bea0d09d58ef50bcfa3a7bf
eliteserving <erimrank167@gmail.com>
2026-06-24 10:26:22 +0300

    Updates

--- LIBRARY/CODE/Connection.js
+++ LIBRARY/CODE/Connection.js
@@ -1,21 +1,26 @@
+import { CINEFYPROJECT } from "./OTHERS/CINEFY/Connection.js";
+import { MAINPROJECT } from "./OTHERS/MAIN/Connection.js";
+import { NAWERITENATPROJECT } from "./OTHERS/NAWERITENAT/Connection.js";
+import { NOORPROJECT } from "./OTHERS/NOOR/Connection.js";
 import { FEATURESPROJECT } from "./OTHERS/PROJECT/Connection.js";
 import { SHYLANSIRAJPROJECT } from "./OTHERS/SHYLANSIRAJ/Connection.js";
+import { TAFYZPROJECT } from "./OTHERS/TAFYZ/Connection.js";
 
 
 export const FEATURES=()=>{
     const TITLE = document.title;
     switch (TITLE) {
-        case "Elite_Robust_Ontology":import(BUILDLINK+"MAIN"+"/Connection.js");
+        case "Elite_Robust_Ontology":MAINPROJECT();
             break;
-        case "Cinefy":import(BUILDLINK+"CINEFY"+"/Connection.js");
+        case "Cinefy":CINEFYPROJECT();
             break;
-        case "Noor_Charity_And_Orphanage_Organisation":import(BUILDLINK+"NOOR"+"/Connection.js");
+        case "Noor_Charity_And_Orphanage_Organisation":NOORPROJECT();
             break;
-        case "Tafyz_Craft_Limited":import(BUILDLINK+"TAFYZ"+"/Connection.js");
+        case "Tafyz_Craft_Limited":TAFYZPROJECT();
             break;
         case "ShyLanSirajFashionHub":SHYLANSIRAJPROJECT();
             break;
-        case "NaweriTenat":import(BUILDLINK+"NAWERITENAT"+"/Connection.js");
+        case "NaweriTenat":NAWERITENATPROJECT();
             break;
         default:FEATURESPROJECT();
             break;
