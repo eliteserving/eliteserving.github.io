a2618478dfb8595c6978efb929408924351ffc1f
eliteserving <erimrank167@gmail.com>
2026-06-24 08:05:13 +0300

    Updates

--- LIBRARY/CODE/Connection.js
+++ LIBRARY/CODE/Connection.js
@@ -1,12 +1,18 @@
 export const FEATURES=()=>{
     const TITLE = document.title;
-    const BUILDLINK="https://eliteserving.github.io/BUILD/";
+    const BUILDLINK="https://eliteserving.github.io/LIBRARY/CODE/OTHERS/";
     switch (TITLE) {
-        case "ANDROID":import(BUILDLINK+NAME+"/ANDROID/index.js");
+        case "Elite_Robust_Ontology":import(BUILDLINK+"MAIN"+"/Connection.js");
             break;
-        case "DESKTOP":import(BUILDLINK+NAME+"/DESKTOP/index.js");
+        case "Cinefy":import(BUILDLINK+"CINEFY"+"/Connection.js");
             break;
-        case "WEB":import(BUILDLINK+NAME+"/WEB/index.js");
+        case "Noor_Charity_And_Orphanage_Organisation":import(BUILDLINK+"NOOR"+"/Connection.js");
+            break;
+        case "Tafyz_Craft_Limited":import(BUILDLINK+"TAFYZ"+"/Connection.js");
+            break;
+        case "ShyLanSirajFashionHub":import(BUILDLINK+"SHYLANSIRAJ"+"/Connection.js");
+            break;
+        case "NaweriTenat":import(BUILDLINK+"NAWERITENAT"+"/Connection.js");
             break;
         default:import("./OTHERS/PROJECT/Connection.js");
             break;
