25333a5be36032f609fd3307bae99c70ad1b5e07
eliteserving <erimrank167@gmail.com>
2026-06-24 07:11:56 +0300

    Updates

--- LIBRARY/TEMPLATES/DATABASE/PAGES/FOOTERVIEWSPAGE/index.js
+++ LIBRARY/TEMPLATES/DATABASE/PAGES/FOOTERVIEWSPAGE/index.js
@@ -1,17 +1,13 @@
 export const FOOTERVIEWSPAGE=(CALLBACK,CALLBACKED)=>{
-
     CLEAR("");
-
     VIEWS("",(HOLDER)=>{
-
+        POSITION(HOLDER,"absolute");
+        BOTTOM(HOLDER,"50px");
+        HEIGHT(HOLDER,"auto");
+        TOP(HOLDER,"0");
         CALLBACK(HOLDER);
-
     });
-
     FOOTER("",(HOLDER)=>{
-
         CALLBACKED(HOLDER);
-
     });
-
 };
--- LIBRARY/TEMPLATES/DATABASE/PAGES/HEADERVIEWSPAGE/index.js
+++ LIBRARY/TEMPLATES/DATABASE/PAGES/HEADERVIEWSPAGE/index.js
@@ -1,17 +1,13 @@
-export const HEADERVIEWSPAGE=(CALLBACK,CALLBACKED)=>{
-
+export const HEADERVIEWSPAGE=(CALLBACKED,CALLBACK)=>{
     CLEAR("");
-
     HEAD("",(HOLDER)=>{
-
-        CALLBACK(HOLDER);
-
-    });
-
-    VIEWS("",(HOLDER)=>{
-
         CALLBACKED(HOLDER);
-
     });
-
+    VIEWS("",(HOLDER)=>{
+        POSITION(HOLDER,"absolute");
+        BOTTOM(HOLDER,"0px");
+        HEIGHT(HOLDER,"auto");
+        TOP(HOLDER,"50px");
+        CALLBACK(HOLDER);
+    });
 };
