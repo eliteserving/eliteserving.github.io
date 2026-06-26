c485de2a2895c07c12d94e9f1ea0efb774a2e55f
eliteserving <erimrank167@gmail.com>
2026-06-26 20:32:13 +0300

    Updates

--- LIBRARY/SERVER/DATABASE/UPLOADFILE/index.js
+++ LIBRARY/SERVER/DATABASE/UPLOADFILE/index.js
@@ -1,16 +1,18 @@
 export const UPLOADFILE=(NAME,BASEFILE,TYPE,CALLBACK)=>{
-
     const DATA = {
         id: "UPLOAD-001",
         fileName: NAME||"Upload.jpg",
         mimeType:TYPE||"image/jpeg",
         file: BASEFILE
     };
-
     FETCH(GOOGLEDRIVEAPI,DATA,(Data)=>{
-
-        CALLBACK(Data);
-
+        console.log(Data)
+        const HEADERS=["DATABASENAME","DATABASELINK","DATE","ACCESS","ADMIN"];
+        const INFO=[Data.fileId,Data.fileUrl,new Date(),"Approved","Elite"];
+        INSERTDATA(MAINCONNECTIONAPI,"UPLOADS",HEADERS,INFO,(InsBack)=>{
+            CHECK(Data.error === false,()=>{
+                CALLBACK("File Uploaded Successfully");
+            });
+        });
     });
-    
 };
