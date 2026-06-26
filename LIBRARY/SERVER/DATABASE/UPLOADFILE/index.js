751cba34d1895f5848b113d80098301ec4ab41b8
eliteserving <erimrank167@gmail.com>
2026-06-26 17:16:07 +0300

    Updates

--- LIBRARY/SERVER/DATABASE/UPLOADFILE/index.js
+++ LIBRARY/SERVER/DATABASE/UPLOADFILE/index.js
@@ -1,5 +1,16 @@
-export const UPLOADFILE=()=>{
-    
-    
+export const UPLOADFILE=(NAME,BASEFILE,TYPE,CALLBACK)=>{
 
+    const DATA = {
+        id: "UPLOAD-001",
+        fileName: NAME||"Upload.jpg",
+        mimeType:TYPE||"image/jpeg",
+        file: BASEFILE
+    };
+
+    FETCH(GOOGLEDRIVEAPI,DATA,(Data)=>{
+
+        CALLBACK(Data);
+
+    });
+    
 };
