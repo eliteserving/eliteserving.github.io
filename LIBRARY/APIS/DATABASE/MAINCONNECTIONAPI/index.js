73b70d9b5ce6c4bacf1025afa1c7ba085570a90c
eliteserving <erimrank167@gmail.com>
2026-06-26 19:34:49 +0300

    Updates

--- LIBRARY/APIS/DATABASE/MAINCONNECTIONAPI/index.js
+++ LIBRARY/APIS/DATABASE/MAINCONNECTIONAPI/index.js
@@ -1,7 +1,7 @@
 let MAINCONNECTIONAPI;
 if (localStorage.getItem("Native")) {
-    MAINCONNECTIONAPI = "'https://docs.google.com/spreadsheets/d/1MNdU8d_brfzTPfNlhkxjV5Viu_Zjh826TGeog36gLi4/edit?usp=sharing'";
+    MAINCONNECTIONAPI = "'https://docs.google.com/spreadsheets/d/16LFihiUWEqvV5Np064F1MVQiNf9f4d12FPbiRUT73-4/edit?usp=sharing'";
 } else {
-    MAINCONNECTIONAPI = "https://docs.google.com/spreadsheets/d/1MNdU8d_brfzTPfNlhkxjV5Viu_Zjh826TGeog36gLi4/edit?usp=sharing";
+    MAINCONNECTIONAPI = "https://docs.google.com/spreadsheets/d/16LFihiUWEqvV5Np064F1MVQiNf9f4d12FPbiRUT73-4/edit?usp=sharing";
 };
 export { MAINCONNECTIONAPI };
