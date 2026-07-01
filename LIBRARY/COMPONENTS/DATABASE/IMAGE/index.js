9aaba708870cd3cd4514dd5150feb1442baa4496
eliteserving <erimrank167@gmail.com>
2026-07-01 10:16:29 +0300

    Updates

--- LIBRARY/COMPONENTS/DATABASE/IMAGE/index.js
+++ LIBRARY/COMPONENTS/DATABASE/IMAGE/index.js
@@ -1,5 +1,6 @@
 export const IMAGE=(HOLDER,SOURCE,CALLBACk)=>{
     ELEMENTED(HOLDER,"img",(DATA)=>{
+        MARGIN(HOLDER,"");
         WIDTH(DATA,"");
         HEIGHT(DATA,"");
         SOURCED(DATA,SOURCE);
