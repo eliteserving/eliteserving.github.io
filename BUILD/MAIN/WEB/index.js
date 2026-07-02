1108d04617a39776e2b33640509f790c88b07b41
eliteserving <erimrank167@gmail.com>
2026-07-02 05:35:01 +0300

    Updates

--- BUILD/MAIN/WEB/index.js
+++ BUILD/MAIN/WEB/index.js
@@ -1,37 +1,11 @@
 const MOBILEVIEW=()=>{
 
-    CLEAR("");
-
-    HEAD("",(HOLDER)=>{
-
-        LEFTICON(HOLDER,ELITELOGO,ELITELOGO,(DATA)=>{
-
-            BORDERRADIUS(DATA,"100%");
-            WIDTH(DATA,"40px");
-            HEIGHT(DATA,"40px");
-
-        });
-
-        HEADER(HOLDER,"Elite Robust Ontology",()=>{
-
-        });
-
-        LEFTICON(HOLDER,WHITEMENUICON,BLACKMENUICON,(DATA)=>{
-
-        });
-
-    });
 
 };
 
 const DESKTOPVIEW=()=>{
 
-    CLEAR("");
-
-    HEADER("","DESKTOP",()=>{
-
-    });
-
+   
 };
 
 VIEW(()=>{MOBILEVIEW();},()=>{DESKTOPVIEW()});
