bd9c9b8045003cf13e385fc4bc8a8445bd1741a3
eliteserving <erimrank167@gmail.com>
2026-07-02 05:56:41 +0300

    Updates

--- BUILD/MAIN/WEB/index.js
+++ BUILD/MAIN/WEB/index.js
@@ -1,11 +1,37 @@
 const MOBILEVIEW=()=>{
 
+  const LOADDATA={
+    home: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    contactus: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    profile: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    saved: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    catergories: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+  }
 
+  LINKTRACKER(LOADDATA,()=>{
+
+    WEBMAINSTARTPAGE();
+
+  });
+   
 };
 
 const DESKTOPVIEW=()=>{
 
-   
+  const LOADDATA={
+    home: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    contactus: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    profile: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    saved: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+    catergories: () => {SHYLANSIRAJANDROIDSTARTPAGE();},
+  }
+
+  LINKTRACKER(LOADDATA,()=>{
+
+    WEBMAINSTARTPAGE();
+
+  });
+    
 };
 
 VIEW(()=>{MOBILEVIEW();},()=>{DESKTOPVIEW()});
