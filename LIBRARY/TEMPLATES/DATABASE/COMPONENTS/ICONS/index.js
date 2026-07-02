3252074d7d4a3f8b9e1c5eb74225225700a39a93
eliteserving <erimrank167@gmail.com>
2026-07-02 06:29:59 +0300

    Updates

--- LIBRARY/TEMPLATES/DATABASE/COMPONENTS/ICONS/index.js
+++ LIBRARY/TEMPLATES/DATABASE/COMPONENTS/ICONS/index.js
@@ -1,13 +1,13 @@
 export const ICONS=(HOLDER,LIGHTICON,DARKICON,CALLBACk)=>{
     CONDITION(sessionStorage.getItem("theme") === "dark",()=>{
-        IMAGE(HOLDER,DARKICON,(DATA)=>{
+        IMAGE(HOLDER,LIGHTICON,(DATA)=>{
             WIDTH(DATA,"20px");
             HEIGHT(DATA,"20px");  
             MARGIN(DATA,"auto");
             CALLBACk(DATA);
         });
     },()=>{
-        IMAGE(HOLDER,LIGHTICON,(DATA)=>{
+        IMAGE(HOLDER,DARKICON,(DATA)=>{
             WIDTH(DATA,"20px");
             HEIGHT(DATA,"20px");  
             MARGIN(DATA,"auto");
