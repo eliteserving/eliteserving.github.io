e5dda1c91fac637012750e90b37edfdc38559d7f
eliteserving <erimrank167@gmail.com>
2026-07-05 11:21:56 +0300

    Updates

--- index.js
+++ index.js
@@ -1,30 +1,69 @@
+const STATUS="";
 const TITLE = document.title;
-if (localStorage.getItem("Native")) {
-    if (TITLE === "EliteBuilder" ) {
-        const script = document.createElement("script");
-        script.type = "module";
-        script.defer=true;
-        script.src = "./CONNECTION/Direct.js";
-        document.head.appendChild(script);
+const RUNNER=()=>{
+    if (localStorage.getItem("Native")) {
+        if (TITLE === "EliteBuilder" ) {
+            const script = document.createElement("script");
+            script.type = "module";
+            script.defer=true;
+            script.src = "./CONNECTION/Direct.js";
+            document.head.appendChild(script);
+        } else {
+            const script = document.createElement("script");
+            script.type = "module";
+            script.defer=true;
+            script.src = "https://eliteserving.github.io/CONNECTION/Direct.js";
+            document.head.appendChild(script);
+        };
     } else {
-        const script = document.createElement("script");
-        script.type = "module";
-        script.defer=true;
-        script.src = "https://eliteserving.github.io/CONNECTION/Direct.js";
-        document.head.appendChild(script);
+        if (TITLE === "EliteBuilder" ) {
+            const script = document.createElement("script");
+            script.type = "module";
+            script.defer=true;
+            script.src = "./CONNECTION/Live.js";
+            document.head.appendChild(script);
+        } else {
+            const script = document.createElement("script");
+            script.type = "module";
+            script.defer=true;
+            script.src = "https://eliteserving.github.io/CONNECTION/Live.js";
+            document.head.appendChild(script);
+        };
     };
+};
+const UPDATES=()=>{
+    let Env;
+    if (localStorage.getItem("ENV") === "ANDROID" ) {
+        Env="Mobile Application";
+    } else {
+        if (localStorage.getItem("ENV") === "DESKTOP") {
+            Env="Desktop Application";  
+        } else {
+            Env="Website";
+        };
+    };
+    let Date="08th July Of 2026";
+    const Body=document.querySelector("body");
+    Body.innerHTML=`
+        <br>
+        <h1>ATTENTION</h1>
+        <br>
+        <h2>Dear Users Of ${TITLE};</h2>
+        <br>
+        <p>This is a Public Announcement That The Services of This ${Env||"Website"} are Temporarily Off Line Undergoing Maintaince and Updates.</p>
+        <br>
+        <p>We are Sorry For Any Inconviences Caused.</p>
+        <br>
+        <h1>Expectations</h1>
+        <p>The ${Env},Will Be Back Online On ${Date}.</p>
+        <br><br>
+        <h1>Elite Robust Ontology</h1>
+        <h2>Development Team </h2>
+        <p>All Rights Reserved.</p>
+    `;
+};
+if (STATUS === "Active" ) {
+    RUNNER();
 } else {
-    if (TITLE === "EliteBuilder" ) {
-        const script = document.createElement("script");
-        script.type = "module";
-        script.defer=true;
-        script.src = "./CONNECTION/Live.js";
-        document.head.appendChild(script);
-    } else {
-        const script = document.createElement("script");
-        script.type = "module";
-        script.defer=true;
-        script.src = "https://eliteserving.github.io/CONNECTION/Live.js";
-        document.head.appendChild(script);
-    };
+    UPDATES();
 };
