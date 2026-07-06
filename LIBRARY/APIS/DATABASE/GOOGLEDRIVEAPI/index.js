915e1a2e323c6e52c13a47972afdce629067fe4b
eliteserving <erimrank167@gmail.com>
2026-07-06 07:58:13 +0300

    Updates

--- LIBRARY/APIS/DATABASE/GOOGLEDRIVEAPI/index.js
+++ LIBRARY/APIS/DATABASE/GOOGLEDRIVEAPI/index.js
@@ -1,7 +1,7 @@
 let GOOGLEDRIVEAPI;
 if (localStorage.getItem("Native")) {
-    GOOGLEDRIVEAPI = "'https://script.google.com/macros/s/AKfycbz-z6Ii-ZqHeAF5Xl8PZoLHeBGK2a75AgkKXKmoEIAbnIX5T9G8GwUcPEmG4y1z6qFo/exec'";
+    GOOGLEDRIVEAPI = "'https://script.google.com/macros/s/AKfycbynmnPfbVqXw3Lrx13ERIB8d97EHnTjYcf_q-eS54govXpwgKe1PhVVung5lqeJCKwv/exec'";
 } else {
-    GOOGLEDRIVEAPI = "https://script.google.com/macros/s/AKfycbz-z6Ii-ZqHeAF5Xl8PZoLHeBGK2a75AgkKXKmoEIAbnIX5T9G8GwUcPEmG4y1z6qFo/exec";
+    GOOGLEDRIVEAPI = "https://script.google.com/macros/s/AKfycbynmnPfbVqXw3Lrx13ERIB8d97EHnTjYcf_q-eS54govXpwgKe1PhVVung5lqeJCKwv/exec";
 };
 export { GOOGLEDRIVEAPI };
