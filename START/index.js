0233950e4c993bf54a180550bcfbb767101fb691
eliteserving <erimrank167@gmail.com>
2026-07-08 17:10:51 +0300

    Updates

--- START/index.js
+++ START/index.js
@@ -62,6 +62,104 @@ const CLOSER=()=>{
 
 };
 
+const Android=document.querySelector("#Android");
+
+Android.addEventListener("click",()=>{
+
+    const SpecialPayments=document.querySelector(".DataHolder");
+
+    SpecialPayments.innerHTML=`
+
+        <br>
+
+        <h1>ANDROID APP DEVELOPMENT</h1>
+
+        <br>
+
+        <p>Elite Robust Onotology ,Android App Development Is Charged As Below.</p>
+
+        <br>
+
+        <h2>Open App</h2>
+
+        <p>Open Apps are Apps thats We Build and You The Owner Host On your Platform.</p>
+        <br>
+        <h4>Price:500,000UGX</h4>
+
+        <br><br>
+
+        <h2>Closed App</h2>
+
+        <p>Closed Apps are Apps thats We Build and Build For it a Land Page Where Your Clients Can Get It.</p>
+        <br>
+        <h4>Price:800,000UGX</h4>
+
+        <br><br>
+
+        <h2>Hosted App</h2>
+
+        <p>Hosted Apps are Apps thats We Build and We Get it Hosted To The Responsible Platforms.</p>
+        <br>
+        <h4>Price:1,000,000UGX</h4>
+
+        <br><br>
+
+        <h1>Payment Options</h1>
+
+        <br>
+
+        <div class="OptionOne">
+
+            <header>
+
+                <h1 class="Names">500,000UGX</h1>
+
+            </header>
+
+            <img src="./android.png"/>
+
+            <button  id="Pay500"  class="Pay">Pay</button>
+
+        </div>
+
+        <br>
+
+        <div class="OptionOne">
+
+            <header>
+
+                <h1 class="Names">800,000UGX</h1>
+
+            </header>
+
+            <img src="./android.png"/>
+
+            <button  id="Pay500"  class="Pay">Pay </button>
+
+        </div>
+
+        <br>
+
+        <div class="OptionOne">
+
+            <header>
+
+                <h1 class="Names">1,000,000UGX</h1>
+
+            </header>
+
+            <img src="./android.png"/>
+
+            <button  id="Pay500"  class="Pay">Pay </button>
+
+        </div>
+
+        <br><br><br><br><br>
+    
+    `;
+
+});
+
 const PAYMENTPROFILE=()=>{
 
 };
