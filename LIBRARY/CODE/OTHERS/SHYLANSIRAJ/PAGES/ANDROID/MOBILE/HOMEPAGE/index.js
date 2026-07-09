386b19dc3f6d597d8e42f7b927a4b0e79057ba65
eliteserving <erimrank167@gmail.com>
2026-07-09 07:10:21 +0300

    Updates

--- LIBRARY/CODE/OTHERS/SHYLANSIRAJ/PAGES/ANDROID/MOBILE/HOMEPAGE/index.js
+++ LIBRARY/CODE/OTHERS/SHYLANSIRAJ/PAGES/ANDROID/MOBILE/HOMEPAGE/index.js
@@ -1,83 +1,43 @@
 export const SHYLANSIRAJANDROIDHOMEPAGE=()=>{
-
     FOOTERVIEWSPAGE((HOLDER)=>{
         LOADINGICON(HOLDER,(LOAD)=>{
             GETINDEX("CLOTHES","CLOTHES",(SDATA)=>{
-                console.log(SDATA)
-                
-            });
-        });
-
-        /*
-
-                        REDUX(DATA,(Element)=>{
-
+                DISPLAYED(LOAD,"none");
+                REDUX(SDATA,(Element)=>{
                     TABLEVIEW(HOLDER,(HOLDEE)=>{
-
                         BACKGROUND(HOLDEE,"gray");
-
                         VIEWS(HOLDEE,(DATA)=>{
-
                             HEIGHT(DATA,"50px");
-
                             BREAK(DATA,()=>{})
-
                             LEFTTEXT(DATA, "UGX : "+Element.AMOUNT);
-
                         });
-
                         IMAGE(HOLDEE,GOOGLEDRIVETHUMBNAILAPI+Element.FILELINK||SHYLANSIRAJLOGO,(IMG)=>{
-
                             HEIGHT(IMG,"200px");
-
                         });
-
                         VIEWS(HOLDEE,(DATA)=>{
-
                             HEIGHT(DATA,"50px");
-
                             LEFTTEXT(DATA,Element.NAME);
-
                         });
 
                     });
-    
                 });
-
-            */
-
+            });
+        });
     },(HOLDER)=>{
-
         ICONTEXT(HOLDER,WHITEGRIDICON,BLACKGRIDICON,"All",(DATA)=>{
-
             CLICK(DATA,()=>{
-
                 ROUTE(" ",ALLSHYLANSIRAJPAGE,"SHYLANSIRAJANDROIDHOMEPAGE");
-
             });
-
         });
-
         ICONTEXT(HOLDER,WHITEHEARTICON,WHITEHEARTICON,"Saved",(DATA)=>{
-
             CLICK(DATA,()=>{
-
                 ROUTE(" ",SHYLANSIRAJANDROIDLIKEDITEMPAGE,"SHYLANSIRAJANDROIDHOMEPAGE");
-
             });
-
         });
-
         ICONTEXT(HOLDER,WHITEUSERICON,BLACKUSERICON,"Profile",(DATA)=>{
-
             CLICK(DATA,()=>{
-
                 ROUTE(" ",SHYLANSIRAJANDROIDPROFILEPAGE,"SHYLANSIRAJANDROIDHOMEPAGE");
-
             });
-
-        });
-        
+        });  
     });
-
 };
