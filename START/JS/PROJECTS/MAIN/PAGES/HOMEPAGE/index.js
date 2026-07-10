668ed8ff319479eb6a8e1b7687b96cf75e74be73
eliteserving <erimrank167@gmail.com>
2026-07-10 23:59:03 +0300

    Updates

---
+++ START/JS/PROJECTS/MAIN/PAGES/HOMEPAGE/index.js
@@ -0,0 +1,19 @@
+export const MAINHOMEPAGE =()=>{
+
+    const Body=document.querySelector("body");
+
+    Body.innerHTML=`
+
+        <br><br>
+
+        <h1>ELITE ROBUST ONTOLOGY</h1>
+
+        <br>
+
+        <p>Welcome To Elite Web Hosting Service Where All Projects Thats Are Hosted Are Displayed HEre</p>
+
+
+    
+    `;
+
+};
