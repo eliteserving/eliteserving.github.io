c70985bb13ca53739768dfb699dfd5ba09207ae6
eliteserving <erimrank167@gmail.com>
2026-07-10 07:41:07 +0300

    Updates

--- LIBRARY/TEMPLATES/DATABASE/FUNCTIONS/POLICIESDOWNLOAD/index.js
+++ LIBRARY/TEMPLATES/DATABASE/FUNCTIONS/POLICIESDOWNLOAD/index.js
@@ -1,27 +1,14 @@
 export const POLICIESDOWNLOAD=(NAME,DATA,CALLBACK)=>{
-
     const TITLE = document.title;
-
     CONDITION(TITLE === "EliteBuilder",()=>{
-
         const LINK="../LIBRARY/POLICIES/"+NAME;
-
         TEXTFETCH(LINK,DATA,(Data)=>{
-
-            CALLBACK(Data);
-            
+            CALLBACK(Data);  
         });
-
     },()=>{
-
         const LINK="https://eliteserving.github.io/LIBRARY/POLICIES/"+NAME;
-
         TEXTFETCH(LINK,DATA,(Data)=>{
-
             CALLBACK(Data);
-            
         });
-
     });
-
 };
