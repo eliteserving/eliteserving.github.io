a0df6404776b10fa0f15f84208bc87f1f8c0f30f
eliteserving <erimrank167@gmail.com>
2026-07-13 10:47:59 +0300

    Updates

--- LIBRARY/CODE/OTHERS/MJOMBA/PAGES/WEB/MOBILE/HOMEPAGE/index.js
+++ LIBRARY/CODE/OTHERS/MJOMBA/PAGES/WEB/MOBILE/HOMEPAGE/index.js
@@ -1,8 +1,21 @@
 export const MJOMBALIWEBMOBILEHOMEPAGE=()=>{
-    HEADERVIEWSPAGE((DATA)=>{
-        LEFTTEXT(DATA,"Doctor Mjomba Ali",()=>{
-        });
-    },()=>{
+    const CSS=`
 
-    });
+        .MjombaLeftText{
+            text-align:left;
+            margin:auto;
+        }
+
+    `;
+    DISPLAY("",`
+
+        <header>
+
+            <h1 class="MjombaLeftText">Doctor Mjomba Ali</h1>
+
+        </header>
+        
+    `);
+    INJECTCSS("https://eliteserving.github.io/START/index.css");
+    INJECTCSS(CSS);
 };
