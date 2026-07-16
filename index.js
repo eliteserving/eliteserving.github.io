d17615bcc5f1bd900976d1632e4898939a874573
eliteserving <erimrank167@gmail.com>
2026-07-16 07:24:36 +0300

    Updates

--- index.js
+++ index.js
@@ -33,41 +33,41 @@ const RUNNER=()=>{
     };
 };
 const UPDATES=()=>{
-    let Env;
-    if (localStorage.getItem("ENV") === "ANDROID" ) {
-        Env="Mobile Application";
+  let Env;
+  if (localStorage.getItem("ENV") === "ANDROID" ) {
+    Env="Mobile Application";
+  } else {
+    if (localStorage.getItem("ENV") === "DESKTOP") {
+      Env="Desktop Application";  
     } else {
-        if (localStorage.getItem("ENV") === "DESKTOP") {
-            Env="Desktop Application";  
-        } else {
-            Env="Website";
-        };
+      Env="Website";
     };
-    let Date="08th July Of 2026";
-    const Body=document.querySelector("body");
-    Body.innerHTML=`
-        <br>
-        <h1 style="color:brown">ATTENTION</h1>
-        <br>
-        <h2>${TITLE} Users:</h2>
-        <br>
-        <p>This is a Public Announcement That The Services of This ${Env||"Website"} are Temporarily Off Line Undergoing Maintaince and Updates.</p>
-        <br>
-        <h1 style="color:teal" >APOLOGIES</h1>
-        <br>
-        <p>We are Sorry For Any Inconviences Caused.</p>
-        <br>
-        <h1 style="color:orange" >EXPECTATIONS</h1>
-        <br>
-        <p>The ${Env},Will Be Back Online On <br> <br> <b style="color:forestgreen">${Date}.</b></p>
-        <br>
-        <h1>Elite Robust Ontology</h1>
-        <h2>Development Team </h2>
-        <p>All Rights Reserved.</p>
-        <br>
-        <a href="mailto:eliterobustontology@gmail.com">Contact Us</a>
-        <br><br><br>
-    `;
+  };
+  let Date="08th July Of 2026";
+  const Body=document.querySelector("body");
+  Body.innerHTML=`
+    <br>
+    <h1 style="color:brown">ATTENTION</h1>
+    <br>
+    <h2>${TITLE} Users:</h2>
+    <br>
+    <p>This is a Public Announcement That The Services of This ${Env||"Website"} are Temporarily Off Line Undergoing Maintaince and Updates.</p>
+    <br>
+    <h1 style="color:teal" >APOLOGIES</h1>
+    <br>
+    <p>We are Sorry For Any Inconviences Caused.</p>
+    <br>
+    <h1 style="color:orange" >EXPECTATIONS</h1>
+    <br>
+    <p>The ${Env},Will Be Back Online On <br> <br> <b style="color:forestgreen">${Date}.</b></p>
+    <br>
+    <h1>Elite Robust Ontology</h1>
+    <h2>Development Team </h2>
+    <p>All Rights Reserved.</p>
+    <br>
+    <a href="mailto:elitehosting09@gmail.com">Contact Us</a>
+    <br><br><br>
+  `;
 };
 async function INSTALLABLE() {
   if (localStorage.getItem("app-installed") === "true") {
@@ -135,10 +135,10 @@ async function INSTALLABLE() {
       }
     );
     window.addEventListener("appinstalled",() => {
-        localStorage.setItem(
-          "app-installed",
-          "true"
-        );
+      localStorage.setItem(
+        "app-installed",
+        "true"
+      );
         INSTALLABLE.prompt = null;
       }
     );
