84eb9098a1358edfbe0e1afd6e6c6766868ec40e
eliteserving <erimrank167@gmail.com>
2026-07-16 07:39:52 +0300

    Updates

---
+++ LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMEIMAGINEPAGE/index.js
@@ -0,0 +1,6 @@
+export const MAINWEBMOBILEHOMEIMAGINEPAGE=(HOLDER)=>{
+    HEADER(HOLDER,"IMAGINE",(DATA)=>{FONTSIZE(DATA,"20px");});
+    BREAK(HOLDER,()=>{});
+    LEFTTEXT(HOLDER,"Most Clients Imagine and Only End There With Future Ideas That Can Change Or Better the Lives Of A Community Or An Individual,So Don't Just Imagine and Sit Back,Let Elite Robust Ontology Get Your Idea From Imagination to Innovations.",()=>{});
+    BREAK(HOLDER,()=>{});  
+};
--- LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMEPAGE/index.js
+++ LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMEPAGE/index.js
@@ -4,10 +4,7 @@ export const WEBMOBILEMAINHOMEPAGE=()=>{
     },(HOLDER)=>{
         VIEWS(HOLDER,(DATA)=>{WIDTH(DATA,"90%");HEIGHT(DATA,"auto");MARGIN(DATA,"5%");BORDERRADIUS(DATA,"10px");IMAGE(DATA,ELITEROBUSTONTOLOGYLOGO,()=>{});});
         MAINWEBMOBILEHOMEWELCOMEPAGE(HOLDER);
-        HEADER(HOLDER,"IMAGINE",(DATA)=>{FONTSIZE(DATA,"20px");});
-      
-        LEFTTEXT(HOLDER,"Most Clients Imagine and Only End There With Future Ideas That Can Change Or Better the Lives Of A Community Or An Individual,So Don't Just Imagine and Sit Back,Let Elite Robust Ontology Get Your Idea From Imagination to Innovations.",()=>{});
-        BREAK(HOLDER,()=>{});
+        MAINWEBMOBILEHOMEIMAGINEPAGE(HOLDER);
         HEADER(HOLDER,"INNOVATE",(DATA)=>{FONTSIZE(DATA,"20px");});
         BREAK(HOLDER,()=>{});
         LEFTTEXT(HOLDER,"Elite Robust Ontology Can Enable You To See Your Dream From the Mind Become A Reality With Our Cloud Native Technology That We Use To Build The Most Influencial Softwares In The World,Lets Innovate The Best Solutions To Problems Using Technology So as We can Change A Person Or Community and Set A Phase Of Inspiration.",()=>{});
