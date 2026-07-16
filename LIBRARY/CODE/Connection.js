16ad9cd00965a5177485f8400786eb05e7115434
eliteserving <erimrank167@gmail.com>
2026-07-16 07:32:40 +0300

    Updates

--- LIBRARY/CODE/Connection.js
+++ LIBRARY/CODE/Connection.js
@@ -3,7 +3,6 @@ import { MAINPROJECT } from "./OTHERS/MAIN/Connection.js";
 import { NOORPROJECT } from "./OTHERS/NOOR/Connection.js";
 import { FEATURESPROJECT } from "./OTHERS/PROJECT/Connection.js";
 import { SHYLANSIRAJPROJECT } from "./OTHERS/SHYLANSIRAJ/Connection.js";
-import { TAFYZPROJECT } from "./OTHERS/TAFYZ/Connection.js";
 
 export const FEATURES=()=>{
     const TITLE = document.title;
@@ -13,8 +12,6 @@ export const FEATURES=()=>{
         case "Cinefy":CINEFYPROJECT();
             break;
         case "Noor_Charity_And_Orphanage_Organisation":NOORPROJECT();
-            break;
-        case "Tafyz_Craft_Limited":TAFYZPROJECT();
             break;
         case "ShyLanSirajFashionHub":SHYLANSIRAJPROJECT();
             break;
--- LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMEWELCOMEPAGE/index.js
+++ LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMEWELCOMEPAGE/index.js
@@ -1,3 +1,4 @@
 export const MAINWEBMOBILEHOMEWELCOMEPAGE=(HOLDER)=>{
+    HEADER(HOLDER,"WELCOME",(DATA)=>{FONTSIZE(DATA,"20px");});
     LEFTTEXT(HOLDER,"Elite Robust Ontology Is A Software Development Company Located In Mbale Uganda Thats Ventures In Development of Mobile Applications,Desktop Applications,Website Development And System Intergrations.",()=>{});
 };
