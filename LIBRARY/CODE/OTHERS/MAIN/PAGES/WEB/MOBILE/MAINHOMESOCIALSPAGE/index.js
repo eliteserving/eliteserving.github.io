62e710d08f6cec362613c4a54cd5a1ce0553cc20
eliteserving <erimrank167@gmail.com>
2026-07-16 13:55:06 +0300

    Updates

--- LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMESOCIALSPAGE/index.js
+++ LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMESOCIALSPAGE/index.js
@@ -6,13 +6,14 @@ export const WEBMAINHOMEMOBILESOCIALMEDIAPAGES=(HOLDER)=>{
             
         });
         ICONTEXT(DATA,COLOREDINSTAGRAM,COLOREDINSTAGRAM,"Instagram",(DATATA)=>{
-
+            CLICK(DATATA,()=>{
+                WEBSITE("https://www.instagram.com/eliterobustontology_?utm_source=qr&igsh=aGI5bXYxOXg3Z2xj");
+            });
         });
         ICONTEXT(DATA,WHITEYOUTUBEICON,BLACKYOUTUBEICON,"YouTube",(DATATA)=>{
             CLICK(DATATA,()=>{
                 WEBSITE("https://www.youtube.com/@eliterobustontology")
-            })
-            
+            });
         });
     });
     HEADER(HOLDER,"ELITE ROBUST ONTOLOGY-2026",(DATA)=>{FONTSIZE(DATA,"20px");});
