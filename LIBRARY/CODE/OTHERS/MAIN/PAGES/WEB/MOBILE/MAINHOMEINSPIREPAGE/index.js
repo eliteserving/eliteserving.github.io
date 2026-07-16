cd5d94fe7d811dfa15182ce82e5c67bbf9676e6a
eliteserving <erimrank167@gmail.com>
2026-07-16 13:18:46 +0300

    Updates

--- LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMEINSPIREPAGE/index.js
+++ LIBRARY/CODE/OTHERS/MAIN/PAGES/WEB/MOBILE/MAINHOMEINSPIREPAGE/index.js
@@ -1,6 +1,6 @@
 export const MAINWEBMOBILEHOMEINSPIREPAGE=(HOLDER)=>{
     HEADER(HOLDER,"INSPIRE",(DATA)=>{FONTSIZE(DATA,"20px");});
     BREAK(HOLDER,()=>{});
-    LEFTTEXT(HOLDER,"Elite Robust Ontology Will Set The Pase For The Rest to Follow After They Seee Our Innovations Because They Are World Changing Revolutionary Types Of Softwares.",()=>{});
+    LEFTTEXT(HOLDER,"Elite Robust Ontology Will Set The Pase For The Rest to Follow After They See Our Innovations Because They Are World Changing Revolutionary Types Of Softwares And One Of The Million Fine Crafts Around The World.",()=>{});
     BREAK(HOLDER,()=>{});  
 };
